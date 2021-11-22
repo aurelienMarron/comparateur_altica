f382ba097f7e6885603d9c1cf581a67478d2b69e
aurelienMarron <aurelien.marron@le-campus-numerique.fr>
2021-11-22 09:50:19 +0100

    Mise en place du titre

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ page_title($pageTitle ?? null) }}</title>
+        <!--<title>{{ page_title($pageTitle ?? null) }}</title>-->
+        <title>Le comparateur de niveaux de services de traduction</title>
 
         <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
